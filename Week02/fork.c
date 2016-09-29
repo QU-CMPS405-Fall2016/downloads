acd1860651750e3101083917bb66739ae48c7492
Ryan Riley <rriley.cs@gmail.com>
2016-09-29 09:50:56 +0300

    Updated version of the fork demo

--- Week02/fork.c
+++ Week02/fork.c
@@ -4,12 +4,25 @@
 int main(void)
 {
 	pid_t ret;
+	int x = 5;
 
 	printf("1\n");
 
 	ret = fork();
 
-	printf("2: %d\n", ret);
+	// I am the child
+	if (ret == 0) {
+		printf("(C) I am the child\n");
+		x = 10;
+		printf("(C) I just changed x to 10\n");
+		printf("(C) x = %d\n", x);
+	}
+	// I am the parent
+	else {
+		sleep(5);
+		printf("(P) I am the parent\n");
+		printf("(P) x = %d\n", x);
+	}
 
 	return 0;
 }
