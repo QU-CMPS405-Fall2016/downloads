92f5c1c772e3328317859a60a938589e2fec6801
Ryan Riley <rriley.cs@gmail.com>
2016-09-29 08:56:05 +0300

    Changing the demo to match what I actually wrote in class

--- Lecture03/address_demo.c
+++ Lecture03/address_demo.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int a = 5;
 
-int adder(int par)
+int func(int par)
 {
-	int c = 20;
-	printf("Address of c:\t%p\n", &c);
-	printf("Address of par:\t%p\n", &par);
-	return par + c;
+	int d = 30;
+	printf("par:\t%p\n",&par);
+	printf("d:\t%p\n",&d);
+	return par;
 }
 
-int main(void)
+int main(void) 
 {
 	int b = 10;
+	int c = 20;
+	int *e = NULL;
 
-	printf("Address of addr function:\t%p\n", &adder);
-	printf("Address of a:\t%p\n", &a);
-	printf("Address of b:\t%p\n", &b);
+	printf("main:\t%p\n", &main);
+	printf("a:\t%p\n", &a);
+	printf("b:\t%p\n", &b);
+	printf("c:\t%p\n", &c);
+	func(13);
 
-	printf("%d\n",a+adder(b));
+	e = malloc(sizeof(int));
+
+	printf("e:\t%p\n", e);
+
+	printf("%d\n", a+b); 
+	return 0;
 }
