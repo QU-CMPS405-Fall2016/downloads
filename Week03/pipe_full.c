47c87e1c3a392e0d9111d5d6a11b411cd1029abb
Ryan Riley <rriley.cs@gmail.com>
2016-10-04 10:50:19 +0300

    Tweaking the pipe full example to include a print

--- Week03/pipe_full.c
+++ Week03/pipe_full.c
@@ -7,7 +7,7 @@ int main()
     int pfds[2];
     char buf[30];
     int n;
-    int i=-1;
+    int i=0;
 
     if (pipe(pfds) == -1) {
         printf("There was an error openning the pipe!\n");
@@ -15,7 +15,9 @@ int main()
     }
 
     do {
-        n = write(pfds[1],"a",1);        
+        n = write(pfds[1],"a",1);
+	i = i + n;
+	printf("i = %d\n",i);
     } while (n == 1);
 
     printf("This will never run because when the pipe is full, write() will block.\n");
