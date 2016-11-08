c06073a29f1f86055fcbb91b769ecf77fe6e4272
Ryan Riley <ryan.riley@qu.edu.qa>
2016-11-08 08:36:55 +0300

    Adding a simple prompt

--- In_Class_Code/Week08/EchoClient.java
+++ In_Class_Code/Week08/EchoClient.java
@@ -31,34 +31,36 @@ public class EchoClient {
 
         // create socket
         int port = 4444;
+
         Socket clientSocket = new Socket("localhost", port);
 
         // Create the reader/write
         BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
-        
+
         // Create the scanner to get input from the user
         Scanner userInput = new Scanner(System.in);
-        
+
         // repeatedly wait for connections, and process
         while (true) {
 
         	// Get a line of input from the user
+        	System.out.print("prompt> ");
         	String inp = userInput.nextLine();
-        	
+
         	// If it is exit, then leave
         	if (inp.equals("exit")) {
         		break;
         	}
-        	      	
+
         	// Send it
         	writer.println(inp);
-        	
+
         	// Read and print the reply
-            String s = reader.readLine();            
+            String s = reader.readLine();
             System.out.println(s);
         }
-        
+
         // close IO streams, then socket
         System.err.println("Closing connection with client");
         writer.close();
